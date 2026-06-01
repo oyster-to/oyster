9d82949de7f35239d72758974e8792c26c6a5763
Matthew Slight <matthew@slight.me>
2026-06-01 17:15:59 +0400

    feat(groovebox): add simple kick/snare fills (snare 4s, 8ths, stutter, kick build)

--- docs/arcade/groovebox/songs/kids.js
+++ docs/arcade/groovebox/songs/kids.js
@@ -38,6 +38,10 @@ export const kids = {
     'snare roll': { kick:[0], snare:[8,9,10,11,12,13,14,15] },
     'crash build':{ kick:[0,8], hat:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], snare:[14,15], crash:[12] },
     'glitch':     { snare:[0,1,2,6,7,10,11,12], kick:[4,8,14], crash:[0] },
+    'snare 4s':   { kick:[0,8], snare:[0,4,8,12] },
+    'snare 8ths': { kick:[0], snare:[0,2,4,6,8,10,12,14] },
+    'snare stutter':{ kick:[0,8], snare:[12,13,14,15] },
+    'kick build': { kick:[0,2,4,6,8,10,12,14], snare:[15] },
   },
   harmony: { progression: [
     { name:'F#m',  root:'F#2', voicing:['F#3','A3','C#4','E4'] },
